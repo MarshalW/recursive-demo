5b470dce6f7e45f4f3091e89cfc9bedc05fa6441
Marshal <marshal.wu@qq.com>
2018-07-13 16:57:43 +0800

    add some search view

---
+++ src/components/SelectApplicationsView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="selected">应用</div>
+</template>
+
+<script>
+export default {
+    name: "SelectApplicationView",
+    data: () => ({
+        selected: false
+    }),
+    mounted () {
+        this.$bus.$on('search.select', (params) => {
+            if (params.type === 'application') {
+                this.selected = true
+            } else {
+                this.selected = false
+            }
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
